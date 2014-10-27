4c70bf7b0fd313a195a48d12a07164179ea5f84a
Aliaksei Piarouski <aliaksei_piarouski@epam.com>
2014-10-27 17:24:57 +0300

    blablabla

--- developing-of-secure-apps/Tests/Program.cs
+++ developing-of-secure-apps/Tests/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +11,11 @@ namespace Tests
     {
         static void Main(string[] args)
         {
-            int x = 9;
-            uint ux = 9;
+            int x = 1;
             Console.WriteLine(x);
             Console.WriteLine();
             Console.WriteLine(Invert4(x));
-            Console.WriteLine(Invert5(ux));
+            Console.WriteLine(Invert4_big(x));
             
             Console.WriteLine();
 
@@ -46,14 +46,36 @@ namespace Tests
             return ~x & (module32 - 1);
         }
 
+        //Наиболее верный вариант
         private static int Invert4(int x)
         {
+            if (x == 0) return 1;
+            if (x < 0) return 0;
+
             var n = Math.Log(x, 2);
             var ceiledN = Math.Ceiling(n);
             var moduleN = (int)Math.Pow(2, ceiledN);
 
             if (Math.Abs(n - ceiledN) > 0)
             {
+                return x ^ (moduleN - 1);
+            }
+            return moduleN - 1;
+        }
+
+        private static BigInteger Invert4_big(BigInteger x)
+        {
+            if (x == 0) return 1;
+            if (x == 1) return 0;
+            if (x < 0) return 0;
+
+            var n = BigInteger.Log(x, 2);
+            var ceiledN = (int) n;
+            var moduleN = BigInteger.Pow(2, ceiledN);
+
+            if (Math.Abs(n - ceiledN) > 0)
+            {
+                moduleN = BigInteger.Pow(2, ceiledN + 1);
                 return x ^ (moduleN - 1);
             }
             return moduleN - 1;
